e32fb177a3946b8373ae9ad8030bc4cc33f7d30f
Bibanaev.A.E <bibanaev.a.e1@sblogistica.ru>
2025-01-22 18:22:39 +0500

    Monolog and Doctrine fixes

--- config/bundles.php
+++ config/bundles.php
@@ -9,4 +9,5 @@ return [
     Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
     SymfonyCasts\Bundle\ResetPassword\SymfonyCastsResetPasswordBundle::class => ['all' => true],
     Snc\RedisBundle\SncRedisBundle::class => ['all' => true],
+    Symfony\Bundle\MonologBundle\MonologBundle::class => ['all' => true],
 ];
